0379f669132d20172945f3f15d79fd59c01180ee
Irfan-Ansari-au25 <irfan398649@gmail.com>
2022-01-23 17:07:30 +0530

    Async & await

--- Interview-JS/AJAX.js
+++ Interview-JS/AJAX.js
@@ -67,3 +67,38 @@ console.log("From AJAX");
 axios.get("https://swapi.dev/api/planets/?page=3").then((res) => {
   console.log(res.data);
 });
+
+// Async returns a resoleved promise
+// use throw for rejected promise
+
+async function greet() {
+  return "hello";
+}
+
+// const getPlanets = () => {
+//   return axios.get("https://swapi.dev/api/planets/?page=3");
+// };
+// getPlanets().then((res) => {
+//   console.log(res.data);
+// });
+
+async function getPlanets() {
+  const res = await axios.get("https://swapi.dev/api/planets/?page=2");
+  console.log(res.data);
+}
+getPlanets().catch((err) => {
+  console.log("In Catch !!");
+  console.log(err);
+});
+
+// async function getPlanets() {
+//   try {
+//     const res = await axios.get("https://swapi.dev/api/planetssdf/?page=2");
+//     console.log(res.data);
+//   } catch (e) {
+//     console.log("In Catch!");
+//     console.log(e);
+//   }
+// }
+
+// getPlanets();
