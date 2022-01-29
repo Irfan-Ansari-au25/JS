0e4aa3d700120cfeb5e5ffed2a66f28b9f81db98
Irfan-Ansari-au25 <irfan398649@gmail.com>
2022-01-30 00:59:32 +0530

    Object copy spread JSON.stringify Object.assign

--- Interview-JS/nestedObjDestructure.js
+++ Interview-JS/nestedObjDestructure.js
@@ -33,3 +33,37 @@ const updatedUser = {
 };
 
 console.log("updated user", updatedUser);
+
+////////////----------------\\\\\\\\\\\\\
+
+const original = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: {
+    aa: 4,
+    bb: 5,
+    cc: 6,
+  },
+  func: function () {
+    console.log("abc");
+  },
+};
+console.log(original, "original");
+// 1- Shallow copy by spread
+const copy = {
+  ...original,
+  d: { ...original.d, aa: 44 },
+};
+console.log(copy, "spread");
+
+// 2- object.assign -> shallow copy
+
+const copy2 = Object.assign({}, original);
+copy2.d.bb = 55;
+console.log(copy2, "ObjectAssign");
+
+// 3- JSON.stringify copies nested but doesn;t copy fn
+const copy3 = JSON.parse(JSON.stringify(original));
+copy3.d.cc = 66;
+console.log(copy3, "stringy");
